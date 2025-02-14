f80936e9f1c81a8f5c4db6abc130d57b0116d5e9
EdgarAOP <202200494@upqroo.edu.mx>
2025-02-14 15:02:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Selección de elementos
     let currentIndex = 0;
     const slides = document.querySelectorAll('.carousel .slide');
     const totalSlides = slides.length;
+    const carousel = document.querySelector('.carousel');
 
-    document.querySelector('.next').addEventListener('click', function() {
+    // Función para actualizar el carrusel
+    function updateCarousel() {
+        const offset = -currentIndex * 110;
+        carousel.style.transform = `translateX(${offset}%)`;
+    }
+
+    // Event Listeners para los botones del carrusel
+    document.querySelector('.next').addEventListener('click', function () {
         currentIndex = (currentIndex + 1) % totalSlides;
         updateCarousel();
     });
 
-    document.querySelector('.prev').addEventListener('click', function() {
+    document.querySelector('.prev').addEventListener('click', function () {
         currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
         updateCarousel();
     });
 
-    function updateCarousel() {
-        const offset = -currentIndex * 110;
-        document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
-    }
+
+});
+
